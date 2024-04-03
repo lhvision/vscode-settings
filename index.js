2b7995e018a8482da5374442ebd1b6a03d370020
lhvision <lhvision@outlook.com>
2024-04-03 17:25:57 +0800

    feat 调整输出信息

--- index.js
+++ index.js
@@ -73,7 +73,7 @@ function colorLog(message, type = 'info') {
       break;
   }
 
-  console.log(color, message, colorANSIMap.reset);
+  console.log(`${color}${message}${colorANSIMap.reset}`);
 }
 
 async function copyDirAsync(configDir, vscodeDir) {
